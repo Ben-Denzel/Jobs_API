f6a33d018ddd8c96f86f235af133cfeda1eb2eaf
Ben Denzel <10bendenzel@gmail.com>
2025-03-08 12:34:10 +0100

    creation of the job API

---
+++ src/main/java/com/backend/vuejobs/VuejobsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.vuejobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuejobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuejobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/vuejobs/VuejobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.vuejobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuejobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
